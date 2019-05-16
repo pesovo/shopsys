83437beb48d5849755bd203790be594478f1b856
Martin Grossmann <martin@vyvoj.net>
2019-05-16 12:49:26 +0200

    fixup! added tests for read-model bundle

--- project-base/tests/ReadModelBundle/Functional/Product/Action/ProductActionViewFacadeTest.php
+++ project-base/tests/ReadModelBundle/Functional/Product/Action/ProductActionViewFacadeTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\ReadModelBundle\Functional\Product\Action;
 
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\ReadModelBundle\Product\Action\ProductActionView;
 use Shopsys\ReadModelBundle\Product\Action\ProductActionViewFacade;
 use Shopsys\ShopBundle\DataFixtures\Demo\ProductDataFixture;
@@ -14,6 +15,8 @@ class ProductActionViewFacadeTest extends FunctionalTestCase
     public function testGetForSingleProduct(): void
     {
         $productActionViewFacade = $this->getContainer()->get(ProductActionViewFacade::class);
+        $domain = $this->getContainer()->get(Domain::class);
+        $url = $domain->getUrl();
 
         $products = [
             $this->getReference(ProductDataFixture::PRODUCT_PREFIX . '1'),
@@ -28,7 +31,7 @@ class ProductActionViewFacadeTest extends FunctionalTestCase
 
         /** @var \Shopsys\ReadModelBundle\Product\Action\ProductActionView[] $productActionViews */
         $this->assertSame(1, $productActionViews[1]->getId());
-        $this->assertSame('http://webserver:8080/22-sencor-sle-22f46dm4-hello-kitty/', $productActionViews[1]->getDetailUrl());
+        $this->assertSame(sprintf('%s/22-sencor-sle-22f46dm4-hello-kitty/', $url), $productActionViews[1]->getDetailUrl());
         $this->assertFalse($productActionViews[1]->isMainVariant());
         $this->assertFalse($productActionViews[1]->isSellingDenied());
     }
--- project-base/tests/ReadModelBundle/Functional/Twig/ImageExtensionTest.php
+++ project-base/tests/ReadModelBundle/Functional/Twig/ImageExtensionTest.php
@@ -7,7 +7,7 @@ namespace Tests\ReadModelBundle\Functional\Twig;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\FrameworkBundle\Component\Image\AdditionalImageData;
 use Shopsys\FrameworkBundle\Component\Image\ImageFacade;
-use Shopsys\FrameworkBundle\Twig\ImageExtension as FrameworkImageExtension;
+use Shopsys\FrameworkBundle\Component\Image\ImageLocator;
 use Shopsys\ReadModelBundle\Image\ImageView;
 use Shopsys\ReadModelBundle\Twig\ImageExtension;
 use Tests\ShopBundle\Test\FunctionalTestCase;
@@ -16,23 +16,25 @@ class ImageExtensionTest extends FunctionalTestCase
 {
     public function testGetImageHtmlWithMockedImageFacade(): void
     {
-        $frameworkBundleImageExtension = $this->getContainer()->get(FrameworkImageExtension::class);
         $domain = $this->getContainer()->get(Domain::class);
+        $imageLocator = $this->getContainer()->get(ImageLocator::class);
+        $templating = $this->getContainer()->get('templating');
 
         $productId = 2;
         $entityName = 'product';
         $extension = 'jpg';
 
-        $imageFacade = $this->createMock(ImageFacade::class);
-        $imageFacade->method('getImageUrlNotUsingImageOrEntity')->willReturn(sprintf('http://webserver:8080/%s/%d.%s', $entityName, $productId, $extension));
-        $imageFacade->method('getAdditionalImagesDataNotUsingImageOrEntity')->willReturn([
+        $imageFacadeMock = $this->createMock(ImageFacade::class);
+        $imageFacadeMock->method('getImageUrlFromAttributes')->willReturn(sprintf('http://webserver:8080/%s/%d.%s', $entityName, $productId, $extension));
+        $imageFacadeMock->method('getAdditionalImagesDataFromAttributes')->willReturn([
             new AdditionalImageData('(min-width: 1200px)', sprintf('http://webserver:8080/%s/additional_0_%d.%s', $entityName, $productId, $extension)),
             new AdditionalImageData('(max-width: 480px)', sprintf('http://webserver:8080/%s/additional_1_%d.%s', $entityName, $productId, $extension)),
         ]);
 
         $imageView = new ImageView($productId, $extension, $entityName, null);
 
-        $readModelBundleImageExtension = new ImageExtension($frameworkBundleImageExtension, $imageFacade, $domain);
+        $readModelBundleImageExtension = new ImageExtension('', $domain, $imageLocator, $imageFacadeMock, $templating);
+
         $html = $readModelBundleImageExtension->getImageHtml($imageView);
 
         $this->assertXmlStringEqualsXmlFile(__DIR__ . '/Resources/picture.twig', $html);
@@ -42,8 +44,10 @@ class ImageExtensionTest extends FunctionalTestCase
 
     public function testGetImageHtml(): void
     {
-        $frameworkBundleImageExtension = $this->getContainer()->get(FrameworkImageExtension::class);
         $domain = $this->getContainer()->get(Domain::class);
+        $imageLocator = $this->getContainer()->get(ImageLocator::class);
+        $templating = $this->getContainer()->get('templating');
+
         $imageFacade = $this->getContainer()->get(ImageFacade::class);
 
         $productId = 1;
@@ -52,7 +56,7 @@ class ImageExtensionTest extends FunctionalTestCase
 
         $imageView = new ImageView($productId, $extension, $entityName, null);
 
-        $readModelBundleImageExtension = new ImageExtension($frameworkBundleImageExtension, $imageFacade, $domain);
+        $readModelBundleImageExtension = new ImageExtension('', $domain, $imageLocator, $imageFacade, $templating);
         $html = $readModelBundleImageExtension->getImageHtml($imageView);
 
         $url = $domain->getCurrentDomainConfig()->getUrl();
@@ -60,6 +64,52 @@ class ImageExtensionTest extends FunctionalTestCase
         $expected = '<picture>';
         $expected .= sprintf('    <source media="(min-width: 480px) and (max-width: 768px)" srcset="%s/content-test/images/product/default/additional_0_1.jpg"/>', $url);
         $expected .= sprintf('    <img alt="" class="image-product" itemprop="image" src="%s/content-test/images/product/default/1.jpg" title=""/>', $url);
+        $expected .= '</picture>';
+
+        $this->assertXmlStringEqualsXmlString($expected, $html);
+
+        libxml_clear_errors();
+    }
+
+    public function testGetNoImageHtml(): void
+    {
+        $domain = $this->getContainer()->get(Domain::class);
+        $imageLocator = $this->getContainer()->get(ImageLocator::class);
+        $templating = $this->getContainer()->get('templating');
+
+        $imageFacade = $this->getContainer()->get(ImageFacade::class);
+
+        $readModelBundleImageExtension = new ImageExtension('', $domain, $imageLocator, $imageFacade, $templating);
+        $html = $readModelBundleImageExtension->getImageHtml(null);
+
+        $url = $domain->getCurrentDomainConfig()->getUrl();
+
+        $expected = '<picture>';
+        $expected .= sprintf('    <img alt="" class="image-noimage" title=""  itemprop="image" src="%s/noimage.png"/>', $url);
+        $expected .= '</picture>';
+
+        $this->assertXmlStringEqualsXmlString($expected, $html);
+
+        libxml_clear_errors();
+    }
+
+    public function testGetNoImageHtmlWithDefaultFrontDesignImageUrlPrefix(): void
+    {
+        $defaultFrontDesignImageUrlPrefix = '/assets/frontend/images/';
+
+        $domain = $this->getContainer()->get(Domain::class);
+        $imageLocator = $this->getContainer()->get(ImageLocator::class);
+        $templating = $this->getContainer()->get('templating');
+
+        $imageFacade = $this->getContainer()->get(ImageFacade::class);
+
+        $readModelBundleImageExtension = new ImageExtension($defaultFrontDesignImageUrlPrefix, $domain, $imageLocator, $imageFacade, $templating);
+        $html = $readModelBundleImageExtension->getImageHtml(null);
+
+        $url = $domain->getCurrentDomainConfig()->getUrl();
+
+        $expected = '<picture>';
+        $expected .= sprintf('    <img alt="" class="image-noimage" title=""  itemprop="image" src="%s%snoimage.png"/>', $url, $defaultFrontDesignImageUrlPrefix);
         $expected .= '</picture>';
 
         $this->assertXmlStringEqualsXmlString($expected, $html);
