db50fb43992fe3429ef148a9418ed7f5350e0959
Martin Grossmann <martin@vyvoj.net>
2019-05-16 12:48:24 +0200

    fixup! FE product lists: read-model bundle is used instead of entities

--- packages/read-model/src/Twig/ImageExtension.php
+++ packages/read-model/src/Twig/ImageExtension.php
@@ -4,52 +4,11 @@ declare(strict_types=1);
 
 namespace Shopsys\ReadModelBundle\Twig;
 
-use Shopsys\FrameworkBundle\Component\Domain\Domain;
-use Shopsys\FrameworkBundle\Component\Image\ImageFacade;
 use Shopsys\FrameworkBundle\Twig\ImageExtension as BaseImageExtension;
 use Shopsys\ReadModelBundle\Image\ImageViewInterface;
-use Twig\Extension\AbstractExtension;
-use Twig\TwigFunction;
 
-class ImageExtension extends AbstractExtension
+class ImageExtension extends BaseImageExtension
 {
-    /**
-     * @var \Shopsys\FrameworkBundle\Twig\ImageExtension
-     */
-    protected $baseImageExtension;
-
-    /**
-     * @var \Shopsys\FrameworkBundle\Component\Image\ImageFacade
-     */
-    protected $imageFacade;
-
-    /**
-     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain
-     */
-    protected $domain;
-
-    /**
-     * @param \Shopsys\FrameworkBundle\Twig\ImageExtension $baseImageExtension
-     * @param \Shopsys\FrameworkBundle\Component\Image\ImageFacade $imageFacade
-     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
-     */
-    public function __construct(BaseImageExtension $baseImageExtension, ImageFacade $imageFacade, Domain $domain)
-    {
-        $this->baseImageExtension = $baseImageExtension;
-        $this->imageFacade = $imageFacade;
-        $this->domain = $domain;
-    }
-
-    /**
-     * @return \Twig\TwigFunction[]
-     */
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('image', [$this, 'getImageHtml'], ['is_safe' => ['html']]),
-        ];
-    }
-
     /**
      * @param \Shopsys\FrameworkBundle\Component\Image\Image|\Shopsys\ReadModelBundle\Image\ImageViewInterface|Object|null $imageView
      * @param array $attributes
@@ -57,37 +16,36 @@ class ImageExtension extends AbstractExtension
      */
     public function getImageHtml($imageView, array $attributes = []): string
     {
-        $this->baseImageExtension->preventDefault($attributes);
-
-        try {
-            if ($imageView instanceof ImageViewInterface) {
-                $attributes['src'] = $this->imageFacade->getImageUrlFromAttributes(
-                    $this->domain->getCurrentDomainConfig(),
-                    $imageView->getId(),
-                    $imageView->getExtension(),
-                    $entityName = $imageView->getEntityName(),
-                    $imageView->getType(),
-                    $attributes['size']
-                );
-                $additionalImagesData = $this->imageFacade->getAdditionalImagesDataFromAttributes(
-                    $this->domain->getCurrentDomainConfig(),
-                    $imageView->getId(),
-                    $imageView->getExtension(),
-                    $entityName = $imageView->getEntityName(),
-                    $imageView->getType(),
-                    $attributes['size']
-                );
-            } elseif ($imageView === null) {
-                throw new \Shopsys\FrameworkBundle\Component\Image\Exception\ImageNotFoundException();
-            } else {
-                return $this->baseImageExtension->getImageHtml($imageView, $attributes);
-            }
-        } catch (\Shopsys\FrameworkBundle\Component\Image\Exception\ImageNotFoundException $e) {
-            $entityName = 'noimage';
-            $attributes['src'] = $this->baseImageExtension->getEmptyImageUrl();
-            $additionalImagesData = [];
+        if ($imageView === null) {
+            return $this->getNoimageHtml($attributes);
         }
 
-        return $this->baseImageExtension->getImageHtmlByEntityName($attributes, $entityName, $additionalImagesData);
+        if ($imageView instanceof ImageViewInterface) {
+            $this->preventDefault($attributes);
+
+            $entityName = $imageView->getEntityName();
+
+            $attributes['src'] = $this->imageFacade->getImageUrlFromAttributes(
+                $this->domain->getCurrentDomainConfig(),
+                $imageView->getId(),
+                $imageView->getExtension(),
+                $entityName,
+                $imageView->getType(),
+                $attributes['size']
+            );
+
+            $additionalImagesData = $this->imageFacade->getAdditionalImagesDataFromAttributes(
+                $this->domain->getCurrentDomainConfig(),
+                $imageView->getId(),
+                $imageView->getExtension(),
+                $entityName,
+                $imageView->getType(),
+                $attributes['size']
+            );
+
+            return $this->getImageHtmlByEntityName($attributes, $entityName, $additionalImagesData);
+        }
+
+        return parent::getImageHtml($imageView, $attributes);
     }
 }
